9a60538e1af1af8cd9497ca8cbd12c93e22f69ec
young6 <devyoungga6@gmail.com>
2018-08-27 19:02:43 +0300

    Basic server version

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const router = express.Router();
-const edge = require('edge.js')
+const edge = require('edge.js');
 const path = require('path');
+const passport = require('passport');
+const User = require('../lib/models/User');
 
 // Home
 router.get('/', (req, res) => {
-    res.send('test');
+  res.send('test');
+});
+
+// @route   GET register
+// @desc    Show registration page
+// @access  Public
+router.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'register.html'));
+});
+
+// @route   POST register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  const errors = {};
+  // Check if email already exists
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      res.status(400).json(errors);
+    } else {
+      // Check if username already taken
+      User.findOne({ name: req.body.username }).then(user => {
+        if (user) {
+          errors.name = 'Choose another username';
+          res.status(400).json(errors);
+        } else {
+          // If no errors, create a new user
+          const newUser = new User({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+          });
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,18 +3,30 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const edge = require('edge.js');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 
 // Static Files (Public)
-app.use(express.static('./public'))
+app.use(express.static('./public'));
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connect to MongoDB
+mongoose
+  .connect(db,  { useNewUrlParser: true } )
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 // Template Engine
-edge.registerViews(path.join(__dirname, './resources/views'))
+edge.registerViews(path.join(__dirname, './resources/views'));
 
 // Router
 app.use('/', require('./routes/routes.js'));
 
 // Listen
 app.listen(process.env.PORT, () => {
-    // Message
-    console.log(`Listening on PORT: ${process.env.PORT}`);
+  // Message
+  console.log(`Listening on PORT: ${process.env.PORT}`);
 });
