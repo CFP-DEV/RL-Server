507cef57092ffde65bb199f5216a1046b3b9c3b6
CFP-DEV <chfrompoland@gmail.com>
2018-09-30 20:36:22 +0200

    Quick Fix 2
    Fixed routes for creating level, fixed fetching single level.

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const levelsController = require('./lib/controllers/LevelsController');
 app.use(express.static('./public'));
 
 // Body parser
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Connect to MongoDB
